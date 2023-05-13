186d229eb2881f719e03c3749fff1c30bb89feb8
ammaciel <adelinemaciel22@gmail.com>
2023-05-13 13:09:53 -0300

    Update 2023-05-13. Add new script and change others

--- caatinga/gdal_process_PRODES_CAATINGA_2000_2021.sh
+++ caatinga/gdal_process_PRODES_CAATINGA_2000_2021.sh
@@ -40,57 +40,58 @@ echo
 echo "Shapefile"
 
 ## CAATINGA
-shapefile="/[path]/shapefiles/limite_caatinga/caatinga_border_4326_newIBGE.shp" # <- CHANGE ME
+#shapefile="/[path]/shapefiles/limite_caatinga/caatinga_border_4326_newIBGE.shp" # <- CHANGE ME
+shapefile="/home/adeline/Dropbox/github_projects/geoserver-mosaics/shapefiles/limite_caatinga/caatinga_border_4326_newIBGE.shp" # <- CHANGE ME
 printf $shapefile
 
-shapefile_grid="/[path]/shapefiles/limite_caatinga/grid_landsat_tm_Caatinga_crop_4326_newIBGE_v2.shp" # <- CHANGE ME
+#shapefile_grid="/[path]/shapefiles/limite_caatinga/grid_landsat_tm_Caatinga_crop_4326_newIBGE_v2.shp" # <- CHANGE ME
+shapefile_grid="/home/adeline/Dropbox/github_projects/geoserver-mosaics/shapefiles/limite_caatinga/grid_landsat_tm_Caatinga_crop_4326_newIBGE_v2.shp" # <- CHANGE ME
 printf $shapefile_grid
 
-rscript_file="/[path]/script_r_cut_images_by_grid_caatinga.R" # <- CHANGE ME
+#rscript_file="/[path]/caatinga/script_r_cut_images_by_grid_caatinga.R" # <- CHANGE ME
+rscript_file="/home/adeline/Dropbox/github_projects/geoserver-mosaics/caatinga/script_r_cut_images_by_grid_pantanal.R" # <- CHANGE ME
 printf $rscript_file
 
-shopt -s nocasematch
+#pyscript_file="/[path]/geoserver-mosaics/sentinel_hist.py" # <- CHANGE ME
+pyscript_file="/home/adeline/Dropbox/github_projects/geoserver-mosaics/sentinel_hist.py" # <- CHANGE ME
+printf $pyscript_file
+
 
 echo
-echo "----- gdal from 16bits to 8bits -----"
+echo "----- gdal from 16bits to 8bits Byte -----"
 echo
-dir=temp8bits
+dir=tempByte
 mkdir -p $dir
 
 for file in *.tif; do
   echo $file
-  gdal_translate -scale -ot Byte -a_nodata 0 -of GTiff "$file" "${dir}/${file}_8bits"
+  gdal_translate -scale -ot Byte -a_nodata 0 -of GTiff "$file" "${dir}/${file}_byte"
   #0 65535 0 255
   echo
 done
 
 echo
-for file1 in $dir/*.tif_8bits; do
-  printf '%s\n' "${file1%.*}_8bits.tif"
-  newname="${file1%.*}_8bits.tif"
+for file1 in $dir/*.tif_byte; do
+  printf '%s\n' "${file1%.*}_byte.tif"
+  newname="${file1%.*}_byte.tif"
   mv -- "$file1" "$newname"
   echo
 done
 echo "Fim: `date +%d-%m-%y_%H:%M:%S`"
 
 echo
-echo "----- gdal NoData -----"
+echo "----- script Cartaxo contraste img Byte input -----"
 echo
-cd temp8bits/
-dir=tempNoData0
+cd tempByte/
+dir=tempPercentile
 mkdir -p $dir
+echo
 
 for file in *.tif; do
-  echo $file
-  gdal_translate -a_nodata 0 -of GTiff "$file" "${dir}/${file}_nodata"
-  echo
-done
-
-echo
-for file1 in $dir/*.tif_nodata; do
-  printf '%s\n' "${file1%.*}_nodata.tif"
-  newname="${file1%.*}_nodata.tif"
-  mv -- "$file1" "$newname"
+  echo "$file"
+  python "$pyscript_file" "$file"
+  newname="${file%.*}_ctx.tif"
+  mv -- "$newname" "$dir"
   echo
 done
 echo "Fim: `date +%d-%m-%y_%H:%M:%S`"
@@ -98,13 +99,13 @@ echo "Fim: `date +%d-%m-%y_%H:%M:%S`"
 echo
 echo "----- gdal EPSG:4326 -----"
 echo
-cd tempNoData0/
+cd tempPercentile/
 dir=tempEPSG4326
 mkdir -p $dir
 
 for file in *.tif; do
   echo $file
-  gdalwarp -dstnodata 0 -t_srs EPSG:4326 -of GTiff "$file" "${dir}/${file}_4326"
+  gdalwarp -t_srs EPSG:4326 -of GTiff "$file" "${dir}/${file}_4326"
   echo
 done
 
@@ -144,7 +145,6 @@ function Rscript_with_status {
 }
 Rscript_with_status
 
-
 echo
 echo "----- gdal Remove Band Alpha -----"
 echo
@@ -167,59 +167,41 @@ for file1 in $dir/*.tif_noalpha; do
 done
 echo "Fim: `date +%d-%m-%y_%H:%M:%S`"
 
-echo "----- Change UInt16 to Byte -----"
+echo
+echo "----- gdal NoData -----"
 echo
 cd tempNoAlphaBand/
-dir=tempByte
+dir=tempNoData
 mkdir -p $dir
 
 for file in *.tif; do
   echo $file
-  gdal_translate -of GTiff -ot Byte -scale -a_nodata 0 "$file" "${dir}/${file}_Byte"
+  gdalwarp -of GTiff -t_srs EPSG:4326 -srcnodata "255 255 255" -dstnodata "0 0 0" "$file" "${dir}/${file}_nodata"
   echo
 done
 
 echo
-for file1 in $dir/*.tif_Byte; do
-  printf '%s\n' "${file1%.*}_Byte.tif"
-  newname="${file1%.*}_Byte.tif"
+for file1 in $dir/*.tif_nodata; do
+  printf '%s\n' "${file1%.*}_nodata.tif"
+  newname="${file1%.*}_nodata.tif"
   mv -- "$file1" "$newname"
   echo
 done
 echo "Fim: `date +%d-%m-%y_%H:%M:%S`"
 
 echo "----- move to trash nodata folder -----"
-mv ${mydir}/temp8bits/tempNoData0/tempEPSG4326/tempCutted_buffer/tempNoAlphaBand/tempByte/ ${mydir}
-#mv ${mydir}/temp8bits/ ~/.local/share/Trash/files/
+mv ${mydir}/tempByte/tempPercentile/tempEPSG4326/tempCutted_buffer/tempNoAlphaBand/tempNoData/ ${mydir}
+#mv ${mydir}/tempByte/ ~/.local/share/Trash/files/
 echo
 
-# # if does not exist some images of the biome
-# echo
-# echo "----- merge all scenes -----"
-# gdal_merge.py -n 0 -a_nodata 0 -of GTiff -o ${mydir}/mosaic_${year}_border.tif ${mydir}/tempByte/*.tif
-# echo
-
-# echo
-# echo "----- gdal cutline -----"
-# gdalwarp -ot Byte -q -of GTiff -srcnodata "0 0 0" -dstalpha -cutline ${shapefile} -crop_to_cutline -co BIGTIFF=YES -co COMPRESS=LZW -wo OPTIMIZE_SIZE=TRUE ${mydir}/mosaic_${year}_border.tif ${mydir}/mosaic_${year}.tif
-# echo
-
-# if exist all images of the biome
-echo
 echo "----- merge all scenes -----"
-gdal_merge.py -n 0 -a_nodata 0 -of GTiff -o ${mydir}/mosaic_${year}_border.tif ${mydir}/tempByte/*.tif
-echo
-echo "Fim: `date +%d-%m-%y_%H:%M:%S`"
-
-echo
-echo "----- change nodata value -----"
-gdalwarp -srcnodata "0 0 0" -dstnodata "0 0 1" ${mydir}/mosaic_${year}_border.tif ${mydir}/mosaic_${year}_border2.tif
+gdal_merge.py -n 0 -a_nodata 0 -of GTiff -o ${mydir}/mosaic_${year}_border.tif ${mydir}/tempNoData/*.tif
 echo
 echo "Fim: `date +%d-%m-%y_%H:%M:%S`"
 
 echo
 echo "----- gdal cutline -----"
-gdalwarp -ot Byte -q -of GTiff -srcnodata "0 0 0" -dstalpha -cutline ${shapefile} -crop_to_cutline -co BIGTIFF=YES -co COMPRESS=LZW -wo OPTIMIZE_SIZE=TRUE ${mydir}/mosaic_${year}_border2.tif ${mydir}/mosaic_${year}.tif
+gdalwarp -ot Byte -q -of GTiff -srcnodata "0 0 0" -dstalpha -cutline ${shapefile} -crop_to_cutline -co BIGTIFF=YES -co COMPRESS=LZW -wo OPTIMIZE_SIZE=TRUE ${mydir}/mosaic_${year}_border.tif ${mydir}/mosaic_${year}.tif
 echo
 echo "Fim: `date +%d-%m-%y_%H:%M:%S`"
 
---
+++ sentinel_hist.py
@@ -0,0 +1,46 @@
+#(base) adeline@adeline-G5-5590:~$ python3 -m pip install --upgrade --no-cache-dir setuptools==58.0.2
+#(base) adeline@adeline-G5-5590:~$ python3 -m pip install --no-cache-dir pygdal==3.0.4.*
+# (base) adeline@adeline-G5-5590:~/mosaicos_terraclass$ python sentinel_hist.py /home/adeline/Downloads/cerrado_agua/mosaico_22869.tif
+# Case: from osgeo import gdal ModuleNotFoundError: No module named 'osgeo'
+# -- pip install --global-option=build_ext --global-option="-I/usr/include/gdal" GDAL==`gdal-config --version`
+# -- sometimes needed sudo apt install libgdal-dev first
+# Script developmented by Cartaxo 2020-08-23
+
+import os,sys
+from osgeo import gdal
+from osgeo import osr
+from osgeo import ogr
+from osgeo.gdalconst import *
+import numpy
+
+driver = gdal.GetDriverByName('GTiff')
+
+ifile = sys.argv[1]
+idataset = gdal.Open(ifile,GA_ReadOnly)
+if idataset is None:
+	print ('\nCould not open ',ifile)
+	exit(1)
+
+ofile = ifile.replace('.tif','_ctx.tif')
+
+
+odataset = driver.Create( ofile, idataset.RasterXSize, idataset.RasterYSize, 3, gdal.GDT_Byte,  options = [ 'BIGTIFF=YES'])
+# Set the geo-transform to the dataset
+odataset.SetGeoTransform(idataset.GetGeoTransform())
+odataset.SetProjection(idataset.GetProjection())
+
+for iband in [1,2,3]:
+	raster = idataset.GetRasterBand(iband).ReadAsArray(0, 0, idataset.RasterXSize, idataset.RasterYSize).astype(numpy.float32)
+	p2, p98 = numpy.percentile(raster[raster>0], (2, 98))
+	print ('band {} p2 {} - p98 {}'.format(iband,p2,p98))
+
+	raster = (raster - p2)/(p98-p2).astype(numpy.float32)
+	raster[raster<0.] = 0.
+	raster[raster>1.] = 1.
+	raster = (255.*raster).astype(numpy.uint8)
+
+	odataset.GetRasterBand(iband).WriteArray(raster)
+
+idataset = None
+odataset = None
+
